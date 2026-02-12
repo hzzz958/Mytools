72056beac46f8045b26e22d669f20a9d28f53dd7
hzzz958 <hkzeo35@gmail.com>
2026-02-13 00:23:02 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,17 +3,15 @@
 支持多个节点模块
 """
 
-# 导入所有节点模块
 from .ffmpeg_node import NODE_CLASS_MAPPINGS as FFmpegMappings
 from .ffmpeg_node import NODE_DISPLAY_NAME_MAPPINGS as FFmpegNames
-
-# 导入 RIFE 节点（强制导入，不再使用 try-except，方便你在控制台看到路径报错）
 from .rife_node import NODE_CLASS_MAPPINGS as RifeMappings
 from .rife_node import NODE_DISPLAY_NAME_MAPPINGS as RifeNames
+from .ffmpeg_concat_node import NODE_CLASS_MAPPINGS as ConcatMappings
+from .ffmpeg_concat_node import NODE_DISPLAY_NAME_MAPPINGS as ConcatNames
 
 # 合并所有节点
-NODE_CLASS_MAPPINGS = {**FFmpegMappings, **RifeMappings}
-NODE_DISPLAY_NAME_MAPPINGS = {**FFmpegNames, **RifeNames}
+NODE_CLASS_MAPPINGS = {**FFmpegMappings, **RifeMappings, **ConcatMappings}
+NODE_DISPLAY_NAME_MAPPINGS = {**FFmpegNames, **RifeNames, **ConcatNames}
 
-# 导出
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
